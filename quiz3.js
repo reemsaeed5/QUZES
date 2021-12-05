ff418d133e6dab8e42e2081300c96eecdd1899b5
reemsaeed5 <reem.saeed.eam.js@tuwaiq.edu.sa>
2021-12-05 10:15:58 +0300

    .

---
+++ quiz3.js
@@ -0,0 +1,56 @@
+//1-create a function longestWord that returns the longest word in a sentence 
+
+ const longestWord = function(){ 
+     const longestWord = ['time dog cat'];
+    let resualt = "";
+    for (let i = 0; i < arr.length; i++) {
+      let current = ar[i];
+      if (current.length > resualt.length) {
+        resualt = ar[i];
+      }
+    }
+    return resualt;
+  }
+  console.log(longest(arr));
+
+  2//  create a function SumOddNumber that returns the sum of all od numbers inside an array of numbers */
+const sumOddNumber = function(arr){}
+let resualt = "";
+    for (let i = 0; i < arr.length; i++) {
+      let current = ar[i];
+      if ([]){
+          console.log}
+        resualt = ar[i];
+      }
+    
+    return resualt;
+  
+//SumOddNumber([1, 2, 3, 4, 5]) // => 9
+
+
+/* 6- Create a function containsElements that takes two arrays of numbers and returns true if the second array contains elements of the numbers of the first array numbers */
+
+const containsElements = function(arr1, arr2){
+    /*         write your code here        */
+    }
+    
+    /* 
+    Examples:
+    containsElements([12, 14, 35], [5, 4, 3, 2, 7]) // => true
+    containsElements([52, 21, 7, 77], [26, 7, 3]) // => false
+    */
+    const containsElements = function(arr1, arr2){
+        containsElements([52, 21, 7, 77], [26, 7, 3])
+        for(let i=0; i ;i++){
+
+
+        }}
+
+
+        // Create a function removeNotUnique that takes a string, delete any characters that are not unique from the string. */
+
+const removeNotUnique = function(str){
+    removeNotUnique('memory');
+    for (let i = 0; i = char.length; i++) {
+}
+}
